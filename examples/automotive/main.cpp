718ae556ab28e5ef156cd8b6fa59c47175639e10
Uwe Rathmann <Uwe.Rathmann@tigertal.de>
2017-07-26 11:56:19 +0200

    using the raster paint engine for the SVGs

--- examples/automotive/main.cpp
+++ examples/automotive/main.cpp
@@ -14,6 +14,19 @@ int main( int argc, char** argv )
     SkinFactory skinFactory;
     Qsk::registerSkinFactory( "SampleSkinFactory", &skinFactory );
 
+    /*
+        When going over QPainter for the SVGs we prefer the raster paint
+        engine, simply showing better results. Interestingly the OpenGL paint
+        engine was even slower for the use case of non-complex SVGs.
+        Looks like its tesselation is not much faster than "pixeling"
+        those images directly.
+     */
+    qskSetup->setControlFlag( QskSetup::PreferRasterForTextures, true );
+
+    // Starting with a simple skin  made for this example
+    // CTRL-S allow to rotate through the registered skins and CTRL-T
+    // changes the colors, when the DefaultSkin is active.
+
     qskSetup->setSkin( "DefaultSkin" );
 
     SkinnyShortcut::enable( SkinnyShortcut::DebugBackground | SkinnyShortcut::Quit );
