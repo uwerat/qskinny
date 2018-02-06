361ecbb980f111006a7dfcac0e4058a04dbbf9d9
Uwe Rathmann <Uwe.Rathmann@tigertal.de>
2018-02-06 14:55:35 +0100

    copyrights added/fixed

--- inputcontext/QskInputCompositionModel.cpp
+++ inputcontext/QskInputCompositionModel.cpp
@@ -1,3 +1,8 @@
+/******************************************************************************
+ * QSkinny - Copyright (C) 2016 Uwe Rathmann
+ * This file may be used under the terms of the QSkinny License, Version 1.0
+ *****************************************************************************/
+
 #include "QskInputCompositionModel.h"
 
 #include <QGuiApplication>
--- inputcontext/QskInputContext.cpp
+++ inputcontext/QskInputContext.cpp
@@ -1,3 +1,8 @@
+/******************************************************************************
+ * QSkinny - Copyright (C) 2016 Uwe Rathmann
+ * This file may be used under the terms of the QSkinny License, Version 1.0
+ *****************************************************************************/
+
 #include "QskInputContext.h"
 
 #include "QskInputCompositionModel.h"
