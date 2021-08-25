c62d85cda8cee7128aac673936cf7b6b9115e905
Uwe Rathmann <Uwe.Rathmann@tigertal.de>
2021-08-25 15:46:08 +0200

    leftover code removed

--- examples/iotdashboard/PieChartPainted.h
+++ examples/iotdashboard/PieChartPainted.h
@@ -6,27 +6,13 @@
 #pragma once
 
 #include <QskControl.h>
-#include "CircularProgressBar.h"
-
-class ProgressBarAnimator;
-class QskTextLabel;
 
 class PieChartPainted : public QskControl
 {
   public:
-    QSK_SUBCONTROLS( Panel )
-
     PieChartPainted( const QColor&, const QskGradient&,
-        int progress, int value, QQuickItem* parent = nullptr );
+        int progress, QQuickItem* parent = nullptr );
 
   protected:
     QSizeF contentsSizeHint( Qt::SizeHint, const QSizeF& ) const override;
-    void updateLayout() override;
-
-  private:
-    QColor m_color;
-    QskGradient m_gradient;
-    CircularProgressBar* m_progressBar;
-    QskTextLabel* m_progressLabel;
-    ProgressBarAnimator* m_animator;
 };
