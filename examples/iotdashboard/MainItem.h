fc1c40141ce399e8bb83c6151925162f7ea472e3
Peter Hartmann <peter@edelhirsch.io>
2021-09-24 14:47:56 +0200

    IOT dashboard: Use cube effect

---
+++ examples/iotdashboard/MainItem.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <QskControl.h>
+#include <QskPanGestureRecognizer.h>
+
+#include <QQuickWindow>
+
+class QskBox;
+class QskLinearBox;
+class QskStackBox;
+
+class MainItem : public QskControl
+{
+    Q_OBJECT
+
+    public:
+        MainItem( QQuickItem* parent = nullptr );
+
+    protected:
+        bool gestureFilter( QQuickItem*, QEvent* ) override final;
+        void gestureEvent( QskGestureEvent* ) override final;
+
+    private:
+        QskStackBox* m_cube;
+        QskLinearBox* m_mainLayout;
+        QskPanGestureRecognizer m_panRecognizer;
+};
