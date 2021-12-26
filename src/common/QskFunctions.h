0d43fbcc27e74f663c6c4d8b6817d432265485fd
Uwe Rathmann <Uwe.Rathmann@tigertal.de>
2021-12-26 12:12:18 +0100

    qskPlatformIntegration/qskMaybeDesktopPlatform added

--- src/common/QskFunctions.h
+++ src/common/QskFunctions.h
@@ -15,10 +15,13 @@ class QScreen;
 class QFont;
 class QFontMetricsF;
 class QString;
+class QPlatformIntegration;
 
 QSK_EXPORT qreal qskDpiScaled( qreal value );
 QSK_EXPORT qreal qskGlobalScaleFactor();
-QSK_EXPORT bool qskHasPlatformWindowManagement();
+
+QSK_EXPORT bool qskMaybeDesktopPlatform();
+QSK_EXPORT const QPlatformIntegration* qskPlatformIntegration();
 QSK_EXPORT QRect qskPlatformScreenGeometry( const QScreen* );
 
 QSK_EXPORT QRect qskAlignedRect( const QRect& outerRect,
