1eae47aefac5238e9f85e9261e794721f56d1799
Uwe Rathmann <Uwe.Rathmann@tigertal.de>
2022-03-28 21:28:56 +0200

    tracking states of the aspects in a QskSkinHintTable

--- src/controls/QskSkin.cpp
+++ src/controls/QskSkin.cpp
@@ -128,7 +128,6 @@ class QskSkin::PrivateData
     std::unordered_map< const QMetaObject*, SkinletData > skinletMap;
 
     QskSkinHintTable hintTable;
-    QskAspect::States stateMask = QskAspect::AllStates;
 
     std::unordered_map< int, QFont > fonts;
     std::unordered_map< int, QskColorFilter > graphicFilters;
@@ -343,22 +342,6 @@ const int* QskSkin::dialogButtonLayout( Qt::Orientation orientation ) const
     }
 
     return QPlatformDialogHelper::buttonLayout( orientation, policy );
-}
-
-void QskSkin::setStateMask( QskAspect::States mask )
-{
-    for ( auto state : { QskControl::Disabled, QskControl::Hovered, QskControl::Focused } )
-    {
-        if ( mask & state )
-            m_data->stateMask |= state;
-        else
-            m_data->stateMask &= ~state;
-    }
-}
-
-QskAspect::States QskSkin::stateMask() const
-{
-    return m_data->stateMask;
 }
 
 QskSkinlet* QskSkin::skinlet( const QMetaObject* metaObject )
