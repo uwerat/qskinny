49cdc1a029f4b37173efea283a759e38a9f1a8c2
Uwe Rathmann <Uwe.Rathmann@tigertal.de>
2022-04-04 14:14:07 +0200

    formal cheges, check box added to gallery

--- src/controls/QskCheckBoxSkinlet.cpp
+++ src/controls/QskCheckBoxSkinlet.cpp
@@ -1,67 +1,88 @@
+/******************************************************************************
+ * QSkinny - Copyright (C) 2016 Uwe Rathmann
+ * This file may be used under the terms of the QSkinny License, Version 1.0
+ *****************************************************************************/
+
 #include "QskCheckBoxSkinlet.h"
 #include "QskCheckBox.h"
 
 #include <QSGFlatColorMaterial>
 #include <qsgnode.h>
 
-class Tic : public QSGGeometryNode {
-    QSGFlatColorMaterial material;
-    QSGGeometry geometry = QSGGeometry(
-        QSGGeometry::defaultAttributes_Point2D(), 3 );
-    const QRectF& target;
-  public:
-    Tic( const QRectF& rect, const QColor& color ): target( rect ) {
-        geometry.setDrawingMode( QSGGeometry::DrawLineStrip );
-        geometry.setLineWidth( 2 );
-        setGeometry( &geometry );
+namespace
+{
+    class Tick : public QSGGeometryNode
+    {
+      public:
+        Tick( const QRectF& rect, const QColor& color )
+            : m_target( rect )
+            , geometry( QSGGeometry::defaultAttributes_Point2D(), 3 )
+        {
+            geometry.setDrawingMode( QSGGeometry::DrawLineStrip );
+            geometry.setLineWidth( 2 );
+            setGeometry( &geometry );
 
-        material.setColor( color );
-        setMaterial( &material );
+            material.setColor( color );
+            setMaterial( &material );
 
-        markDirty( QSGNode::DirtyGeometry );
-    }
+            markDirty( QSGNode::DirtyGeometry );
+        }
 
-    void setColor( const QColor& color ) {
-        material.setColor( color );
-        markDirty( QSGNode::DirtyMaterial );
-    }
+        void setColor( const QColor& color ) {
+            material.setColor( color );
+            markDirty( QSGNode::DirtyMaterial );
+        }
 
-    void makeTic() {
-        const auto& size = target.size();
-        const auto x = target.x();
-        const auto y = target.y();
+        void makeTick()
+        {
+            const auto x = m_target.x();
+            const auto y = m_target.y();
 
-        geometry.vertexDataAsPoint2D()[0].set( x, y + size.height() / 2 );
-        geometry.vertexDataAsPoint2D()[1].set( x + size.width() / 3,
-            y + size.height() );
-        geometry.vertexDataAsPoint2D()[2].set( x + size.width(), y );
-        markDirty( QSGNode::DirtyGeometry );
-    }
+            auto vertexData = geometry.vertexDataAsPoint2D();
 
-    void makePartially() {
-        const auto& size = target.size();
-        const auto x = target.x();
-        const auto y = target.y();
+            vertexData[0].set( x, y + m_target.height() / 2 );
+            vertexData[1].set( x + m_target.width() / 3, y + m_target.height() );
+            vertexData[2].set( x + m_target.width(), y );
 
-        geometry.vertexDataAsPoint2D()[0].set( x, y + size.height() / 2 );
-        geometry.vertexDataAsPoint2D()[1].set( x, y + size.height() / 2 );
-        geometry.vertexDataAsPoint2D()[2].set( x + size.width(),
-            y + size.height() / 2 );
+            markDirty( QSGNode::DirtyGeometry );
+        }
 
-        markDirty( QSGNode::DirtyGeometry );
-    }
+        void makePartially()
+        {
+            const auto x = m_target.x();
+            const auto y = m_target.y();
 
-    void makeEmpty() {
-        const auto x = target.x();
-        const auto y = target.y();
+            auto vertexData = geometry.vertexDataAsPoint2D();
 
-        geometry.vertexDataAsPoint2D()[0].set( x, y );
-        geometry.vertexDataAsPoint2D()[1].set( x, y );
-        geometry.vertexDataAsPoint2D()[2].set( x, y );
+            vertexData[0].set( x, y + m_target.height() / 2 );
+            vertexData[1].set( x, y + m_target.height() / 2 );
+            vertexData[2].set( x + m_target.width(), y + m_target.height() / 2 );
 
-        markDirty( QSGNode::DirtyGeometry );
-    }
-};
+            markDirty( QSGNode::DirtyGeometry );
+        }
+
+        void makeEmpty()
+        {
+            const auto x = m_target.x();
+            const auto y = m_target.y();
+
+            auto vertexData = geometry.vertexDataAsPoint2D();
+
+            vertexData[0].set( x, y );
+            vertexData[1].set( x, y );
+            vertexData[2].set( x, y );
+
+            markDirty( QSGNode::DirtyGeometry );
+        }
+
+      private:
+
+        const QRectF m_target;
+
+        QSGFlatColorMaterial material;
+        QSGGeometry geometry;
+    };
+}
 
 QskCheckBoxSkinlet::QskCheckBoxSkinlet( QskSkin* skin )
     : QskSkinlet( skin )
@@ -74,9 +95,7 @@ QskCheckBoxSkinlet::~QskCheckBoxSkinlet()
 }
 
 QRectF QskCheckBoxSkinlet::subControlRect(
-    const QskSkinnable*,
-    const QRectF& contentsRect,
-    QskAspect::Subcontrol ) const
+    const QskSkinnable*, const QRectF& contentsRect, QskAspect::Subcontrol ) const
 {
     return contentsRect;
 }
@@ -84,47 +103,60 @@ QRectF QskCheckBoxSkinlet::subControlRect(
 QSGNode* QskCheckBoxSkinlet::updateSubNode(
     const QskSkinnable* skinnable, quint8 nodeRole, QSGNode* node ) const
 {
-    switch( nodeRole ) {
+    switch( nodeRole )
+    {
         case BoxRole:
-            return updateBoxNode( skinnable, node,
-                QskCheckBox::Box );
+        {
+            return updateBoxNode( skinnable, node, QskCheckBox::Box );
+        }
+
         case TickRole:
-            auto control = dynamic_cast< const QskCheckBox* >( skinnable );
-            auto rect = control->subControlRect( QskCheckBox::Tick );
-            rect = rect.marginsRemoved(
-                skinnable->marginHint( QskCheckBox::Tick ) );
-
-            Tic* tic;
-            if ( static_cast< Tic* >( node ) == nullptr )
-            {
-                tic = new Tic( rect, skinnable->color( QskCheckBox::Tick ) );
-            }
-            else
-            {
-                tic = static_cast< Tic* >( node );
-            }
-
-            switch ( control->checkState() ) {
-                case Qt::CheckState::Unchecked:
-                    tic->setColor( skinnable->color( QskCheckBox::Tick ) );
-                    tic->makeEmpty();
-                    break;
-                case Qt::CheckState::PartiallyChecked:
-                    tic->setColor( skinnable->color(
-                        QskCheckBox::Tick | QskCheckBox::PartiallyChecked ) );
-                    tic->makePartially();
-                    break;
-                case Qt::CheckState::Checked:
-                    tic->setColor( skinnable->color(
-                        QskCheckBox::Tick | QskCheckBox::Checked ) );
-                    tic->makeTic();
-                    break;
-            }
-
-            return tic;
+        {
+            auto checkBox = static_cast< const QskCheckBox* >( skinnable );
+            return updateTickNode( checkBox, node );
+        }
     }
 
     return Inherited::updateSubNode( skinnable, nodeRole, node );
+}
+
+QSGNode* QskCheckBoxSkinlet::updateTickNode(
+    const QskCheckBox* checkBox, QSGNode* node ) const
+{
+    using Q = QskCheckBox;
+
+    auto rect = checkBox->subControlRect( Q::Tick );
+    rect = rect.marginsRemoved( checkBox->marginHint( Q::Tick ) );
+
+    auto tick = static_cast< Tick* >( node );
+    if ( tick == nullptr )
+        tick = new Tick( rect, checkBox->color( Q::Tick ) );
+
+    switch ( checkBox->checkState() )
+    {
+        case Qt::Unchecked:
+
+            tick->setColor( checkBox->color( Q::Tick ) );
+            tick->makeEmpty();
+
+            break;
+
+        case Qt::PartiallyChecked:
+
+            tick->setColor( checkBox->color( Q::Tick | Q::PartiallyChecked ) );
+            tick->makePartially();
+
+            break;
+
+        case Qt::Checked:
+
+            tick->setColor( checkBox->color( Q::Tick | Q::Checked ) );
+            tick->makeTick();
+
+            break;
+    }
+
+    return tick;
 }
 
 QSizeF QskCheckBoxSkinlet::sizeHint( const QskSkinnable* skinnable,
