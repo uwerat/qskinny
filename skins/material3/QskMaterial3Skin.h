d79a2b52264222d8ab2eb1d7938ed162c2149529
Peter Hartmann <peter@edelhirsch.io>
2022-07-06 14:48:06 +0200

    Change copyright

--- skins/material3/QskMaterial3Skin.h
+++ skins/material3/QskMaterial3Skin.h
@@ -1,6 +1,6 @@
 /******************************************************************************
- * QSkinny - Copyright (C) 2016 Uwe Rathmann
- * This file may be used under the terms of the QSkinny License, Version 1.0
+ * QSkinny - Copyright (C) 2022 Edelhirsch Software GmbH
+ * This file may be used under the terms of the 3-clause BSD License
  *****************************************************************************/
 
 #ifndef QSK_MATERIAL3_SKIN_H
