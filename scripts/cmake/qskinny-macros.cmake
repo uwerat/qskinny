ce233ccb093d3d673366e51d2f6041ce20f0db6e
Rick Vogel <vrick1991@googlemail.com>
2022-09-29 21:34:04 +0200

    make svg2qvg for robust

--- scripts/cmake/qskinny-macros.cmake
+++ scripts/cmake/qskinny-macros.cmake
@@ -2,6 +2,7 @@ if(TARGET Qt5::Svg)
     ## @param SVG_FILENAME absolute filename to the svg
     ## @param QVG_FILENAME absolute filename to the qvg
     function(svg2qvg SVG_FILENAME QVG_FILENAME)
+        get_filename_component(QVG_FILENAME ${QVG_FILENAME} ABSOLUTE)
         get_filename_component(SVG_FILENAME ${SVG_FILENAME} ABSOLUTE)
         add_custom_command(
             COMMAND svg2qvg ${SVG_FILENAME} ${QVG_FILENAME}
