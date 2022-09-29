f2ec4f862f8322dfd2f88790e9a276825763ae7e
Rick Vogel <vrick1991@googlemail.com>
2022-09-29 21:26:29 +0200

    added svg2qvg cmake function

---
+++ scripts/cmake/qskinny-macros.cmake
@@ -0,0 +1,13 @@
+if(TARGET Qt5::Svg)
+    ## @param SVG_FILENAME absolute filename to the svg
+    ## @param QVG_FILENAME absolute filename to the qvg
+    function(svg2qvg SVG_FILENAME QVG_FILENAME)
+        get_filename_component(SVG_FILENAME ${SVG_FILENAME} ABSOLUTE)
+        add_custom_command(
+            COMMAND svg2qvg ${SVG_FILENAME} ${QVG_FILENAME}
+            OUTPUT ${QVG_FILENAME}
+            DEPENDS ${SVG_FILENAME}
+            WORKING_DIRECTORY $<TARGET_FILE_DIR:Qt5::Svg>
+            COMMENT "Compiling ${SVG_FILENAME} to ${QVG_FILENAME}")
+    endfunction()
+endif()
