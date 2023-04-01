4c61300d6e46b65edc3d34c1f12b2a025e18e791
Uwe Rathmann <Uwe.Rathmann@tigertal.de>
2023-04-01 18:12:36 +0200

    QskTools.cmake introduced

--- cmake/QSkinnyMacros.cmake
+++ cmake/QSkinnyMacros.cmake
@@ -3,21 +3,6 @@
 # This file may be used under the terms of the QSkinny License, Version 1.0
 ############################################################################
 
-if(TARGET ${Qt}::Svg)
-    ## @param SVG_FILENAME absolute filename to the svg
-    ## @param QVG_FILENAME absolute filename to the qvg
-    function(qsk_svg2qvg SVG_FILENAME QVG_FILENAME)
-        get_filename_component(QVG_FILENAME ${QVG_FILENAME} ABSOLUTE)
-        get_filename_component(SVG_FILENAME ${SVG_FILENAME} ABSOLUTE)
-        add_custom_command(
-            COMMAND svg2qvg ${SVG_FILENAME} ${QVG_FILENAME}
-            OUTPUT ${QVG_FILENAME}
-            DEPENDS ${SVG_FILENAME}
-            WORKING_DIRECTORY $<TARGET_FILE_DIR:${Qt}::Svg>
-            COMMENT "Compiling ${SVG_FILENAME} to ${QVG_FILENAME}")
-    endfunction()
-endif()
-
 function(qsk_add_executable target)
 
     if(QT_VERSION_MAJOR VERSION_GREATER_EQUAL 6)
