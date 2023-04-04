b21b42a8be834fa7c7ce791365f14e1aa4f53aec
Uwe Rathmann <Uwe.Rathmann@tigertal.de>
2023-04-04 12:23:46 +0200

    CI fixed ( kudos to Rick )

--- examples/iotdashboard/main.cpp
+++ examples/iotdashboard/main.cpp
@@ -19,6 +19,7 @@
 #include <QskObjectCounter.h>
 
 #include <QGuiApplication>
+#include <QTimer>
 
 namespace
 {
@@ -85,6 +86,17 @@ int main( int argc, char* argv[] )
 
     MainWindow window;
     window.show();
+
+    for( int i = 1; i < argc; i++ )
+    {
+      if( argv[i] == QStringLiteral("--screenshot") && i + 1 < argc )
+      {
+          QTimer::singleShot( 500, &window, [&app, &window, filename = QString(argv[i + 1])]()
+            { auto image = window.grabWindow(); image.save(filename); } );
+
+          break;
+      }
+    }
 
     return app.exec();
 }
