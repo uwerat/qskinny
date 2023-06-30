4a92bc7c12bfca614b132143efd97d1c897090ff
Uwe Rathmann <Uwe.Rathmann@tigertal.de>
2023-06-30 14:40:19 +0200

    listbox example moved into the gallery

---
+++ examples/gallery/listbox/ListBoxPage.cpp
@@ -0,0 +1,60 @@
+/******************************************************************************
+ * QSkinny - Copyright (C) 2016 Uwe Rathmann
+ *           SPDX-License-Identifier: BSD-3-Clause
+ *****************************************************************************/
+
+#include "ListBoxPage.h"
+#include <QskSimpleListBox.h>
+#include <QskFunctions.h>
+
+namespace
+{
+    class ListBox : public QskSimpleListBox
+    {
+      public:
+        ListBox( QQuickItem* parent = nullptr )
+            : QskSimpleListBox( parent )
+        {
+            setMargins( QMarginsF( 15, 10, 10, 10 ) );
+
+            // increasing the padding of each row: usually the job of the skin !
+            setPaddingHint( Cell, QMargins( 10, 20, 10, 20 ) );
+
+            populate();
+
+            setSelectedRow( 5 );
+        }
+
+      private:
+        void populate()
+        {
+            const int count = 10000;
+
+            const QString format( "Row %1: The quick brown fox jumps over the lazy dog" );
+
+            QStringList entries;
+            entries.reserve( count );
+
+            for ( int i = 0; i < count; i++ )
+            {
+                entries += format.arg( i + 1 );
+            }
+
+            // we know, that the last entry is the longest one and
+            // can prevent the list box from having to find it out
+            // the expensive way.
+
+            const qreal maxWidth = qskHorizontalAdvance( effectiveFont( Cell ), entries.last() );
+            setColumnWidthHint( 0, maxWidth );
+
+            append( entries );
+        }
+    };
+}
+
+ListBoxPage::ListBoxPage( QQuickItem* parent )
+    : Page( Qt::Horizontal, parent )
+{
+    setMargins( 5 );
+    (void) new ListBox( this );
+}
--- examples/gallery/main.cpp
+++ examples/gallery/main.cpp
@@ -10,6 +10,7 @@
 #include "button/ButtonPage.h"
 #include "selector/SelectorPage.h"
 #include "dialog/DialogPage.h"
+#include "listbox/ListBoxPage.h"
 
 #include <SkinnyShortcut.h>
 #include <SkinnyShapeProvider.h>
@@ -250,6 +251,7 @@ namespace
             tabView->addPage( "Progress\nBars", new ProgressBarPage() );
             tabView->addPage( "Selectors", new SelectorPage() );
             tabView->addPage( "Dialogs", new DialogPage() );
+            tabView->addPage( "ListBox", new ListBoxPage() );
 
             connect( header, &Header::enabledToggled,
                 tabView, &TabView::setPagesEnabled );
