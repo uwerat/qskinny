9069c8e39084764096bd8e9d41b7ceb158bf43a1
Uwe Rathmann <Uwe.Rathmann@tigertal.de>
2023-10-15 15:33:32 +0200

    position is a progress value [0-1] - nothing in dp

--- skins/material3/QskMaterial3Skin.cpp
+++ skins/material3/QskMaterial3Skin.cpp
@@ -379,7 +379,7 @@ void Editor::setupMenu()
     setFontRole( Q::Text, QskMaterial3Skin::M3BodyMedium );
 
     setPosition( Q::Panel, 0 );
-    setPosition( Q::Panel | QskPopup::Closed, 1_dp );
+    setPosition( Q::Panel | QskPopup::Closed, 1 );
 
     setAnimation( Q::Panel | A::Metric, 150 );
     setAnimation( Q::Cursor | A::Position | A::Metric, 75, QEasingCurve::OutCubic );
