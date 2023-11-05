22a46df12dd7de04a407c12e6ba3a2aa1e51dcb7
Rick Vogel <vrick1991@googlemail.com>
2023-11-05 18:51:56 +0100

    clang-format

--- src/nodes/QskTickmarksMetrics.h
+++ src/nodes/QskTickmarksMetrics.h
@@ -13,32 +13,32 @@
 class QSK_EXPORT QskTickmarksMetrics
 {
     Q_GADGET
-    Q_PROPERTY( qreal majorRatio READ majorRatio WRITE setMajorRatio)
-    Q_PROPERTY( qreal mediumRatio READ mediumRatio WRITE setMediumRatio)
-    Q_PROPERTY( qreal minorRatio READ minorRatio WRITE setMinorRatio)
+    Q_PROPERTY( qreal majorRatio READ majorRatio WRITE setMajorRatio )
+    Q_PROPERTY( qreal mediumRatio READ mediumRatio WRITE setMediumRatio )
+    Q_PROPERTY( qreal minorRatio READ minorRatio WRITE setMinorRatio )
 
   public:
     constexpr QskTickmarksMetrics() noexcept = default;
     constexpr QskTickmarksMetrics( qreal minorRatio, qreal mediumRatio, qreal majorRatio ) noexcept;
-    constexpr QskTickmarksMetrics(const QskTickmarksMetrics&) noexcept = default;
+    constexpr QskTickmarksMetrics( const QskTickmarksMetrics& ) noexcept = default;
     constexpr QskTickmarksMetrics( QskTickmarksMetrics&& ) noexcept = default;
 
-    constexpr QskTickmarksMetrics& operator=(const QskTickmarksMetrics&) noexcept = default;
-    constexpr QskTickmarksMetrics& operator=(QskTickmarksMetrics&&) noexcept = default;
+    constexpr QskTickmarksMetrics& operator=( const QskTickmarksMetrics& ) noexcept = default;
+    constexpr QskTickmarksMetrics& operator=( QskTickmarksMetrics&& ) noexcept = default;
 
-    constexpr Q_REQUIRED_RESULT bool operator==(const QskTickmarksMetrics& rhs) const noexcept;
-    constexpr Q_REQUIRED_RESULT bool operator!=(const QskTickmarksMetrics& rhs) const noexcept;
+    constexpr Q_REQUIRED_RESULT bool operator==( const QskTickmarksMetrics& rhs ) const noexcept;
+    constexpr Q_REQUIRED_RESULT bool operator!=( const QskTickmarksMetrics& rhs ) const noexcept;
 
     constexpr Q_REQUIRED_RESULT qreal majorRatio() const noexcept;
-    constexpr Q_REQUIRED_RESULT qreal mediumRatio() const noexcept;    
+    constexpr Q_REQUIRED_RESULT qreal mediumRatio() const noexcept;
     constexpr Q_REQUIRED_RESULT qreal minorRatio() const noexcept;
 
     constexpr void setMajorRatio( qreal ratio ) noexcept;
     constexpr void setMediumRatio( qreal ratio ) noexcept;
-    constexpr void setMinorRatio( qreal ratio ) noexcept;    
+    constexpr void setMinorRatio( qreal ratio ) noexcept;
 
-    constexpr Q_REQUIRED_RESULT qreal operator[](int index) const noexcept;
-    constexpr Q_REQUIRED_RESULT qreal& operator[](int index) noexcept;
+    constexpr Q_REQUIRED_RESULT qreal operator[]( int index ) const noexcept;
+    constexpr Q_REQUIRED_RESULT qreal& operator[]( int index ) noexcept;
 
   private:
     qreal m_minorRatio = 0.7;
@@ -46,8 +46,11 @@ class QSK_EXPORT QskTickmarksMetrics
     qreal m_majorRatio = 1.0;
 };
 
-inline constexpr QskTickmarksMetrics::QskTickmarksMetrics(qreal minorRatio, qreal mediumRatio, qreal majorRatio) noexcept
-    : m_minorRatio(minorRatio), m_mediumRatio(mediumRatio), m_majorRatio(majorRatio)
+inline constexpr QskTickmarksMetrics::QskTickmarksMetrics(
+    qreal minorRatio, qreal mediumRatio, qreal majorRatio ) noexcept
+    : m_minorRatio( minorRatio )
+    , m_mediumRatio( mediumRatio )
+    , m_majorRatio( majorRatio )
 {
 }
 
@@ -59,8 +62,8 @@ inline constexpr qreal QskTickmarksMetrics::majorRatio() const noexcept
 inline constexpr qreal QskTickmarksMetrics::mediumRatio() const noexcept
 {
     return m_mediumRatio;
-} 
-    
+}
+
 inline constexpr qreal QskTickmarksMetrics::minorRatio() const noexcept
 {
     return m_minorRatio;
@@ -81,19 +84,21 @@ inline constexpr void QskTickmarksMetrics::setMinorRatio( qreal ratio ) noexcept
     m_minorRatio = qBound( 0.0, ratio, 1.0 );
 }
 
-inline constexpr bool QskTickmarksMetrics::operator==(const QskTickmarksMetrics& rhs) const noexcept
+inline constexpr bool QskTickmarksMetrics::operator==(
+    const QskTickmarksMetrics& rhs ) const noexcept
 {
-    return qskFuzzyCompare(m_minorRatio, rhs.m_minorRatio) &&
-           qskFuzzyCompare(m_mediumRatio, rhs.m_mediumRatio) &&
-           qskFuzzyCompare(m_majorRatio, rhs.m_majorRatio);
+    return qskFuzzyCompare( m_minorRatio, rhs.m_minorRatio ) &&
+           qskFuzzyCompare( m_mediumRatio, rhs.m_mediumRatio ) &&
+           qskFuzzyCompare( m_majorRatio, rhs.m_majorRatio );
 }
 
-inline constexpr bool QskTickmarksMetrics::operator!=(const QskTickmarksMetrics& rhs) const noexcept
+inline constexpr bool QskTickmarksMetrics::operator!=(
+    const QskTickmarksMetrics& rhs ) const noexcept
 {
-    return !(*this == rhs);
+    return !( *this == rhs );
 }
 
-inline constexpr qreal QskTickmarksMetrics::operator[](int index) const noexcept
+inline constexpr qreal QskTickmarksMetrics::operator[]( int index ) const noexcept
 {
     index = index % 3;
 
@@ -108,7 +113,7 @@ inline constexpr qreal QskTickmarksMetrics::operator[](int index) const noexcept
     }
 }
 
-inline constexpr qreal& QskTickmarksMetrics::operator[](int index) noexcept
+inline constexpr qreal& QskTickmarksMetrics::operator[]( int index ) noexcept
 {
     index = index % 3;
 
