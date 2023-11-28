e67ee712248701c6acfc49987e67b23f94bbc668
Uwe Rathmann <Uwe.Rathmann@tigertal.de>
2023-11-28 15:47:33 +0100

    qml support for QskGraduationMetrics added

--- qmlexport/QskQml.cpp
+++ qmlexport/QskQml.cpp
@@ -23,6 +23,7 @@
 #include <QskFocusIndicator.h>
 #include <QskGradient.h>
 #include <QskGradientDirection.h>
+#include <QskGraduationMetrics.h>
 #include <QskGraphicLabel.h>
 #include <QskIcon.h>
 #include <QskIntervalF.h>
@@ -252,6 +253,7 @@ void QskQml::registerTypes()
 
     registerGadget< QskBoxBorderMetrics >();
     registerGadget< QskBoxShapeMetrics >();
+    registerGadget< QskGraduationMetrics >();
     registerGadget< QskShadowMetrics >();
     registerGadget< QskIntervalF >();
     registerGadget< QskLayoutMetrics >();
