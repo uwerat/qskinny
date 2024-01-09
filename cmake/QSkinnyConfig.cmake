62b5ef1323279c354a0cbafe615544adfa49b530
Rick Vogel <vrick1991@googlemail.com>
2024-01-10 00:33:35 +0100

    renamed exported targets and files

--- cmake/QSkinnyConfig.cmake
+++ cmake/QSkinnyConfig.cmake
@@ -1,3 +1,3 @@
 include("${CMAKE_CURRENT_LIST_DIR}/QSkinnyTargets.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/QskTools.cmake" OPTIONAL)
-include("${CMAKE_CURRENT_LIST_DIR}/QmlExportTargets.cmake" OPTIONAL)
+include("${CMAKE_CURRENT_LIST_DIR}/QSkinnyTools.cmake" OPTIONAL)
+include("${CMAKE_CURRENT_LIST_DIR}/QSkinnyQmlExportTargets.cmake" OPTIONAL)
