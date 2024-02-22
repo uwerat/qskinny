39620bc8f02e0a6a4cb0bbaa868a364c6437f338
Uwe Rathmann <Uwe.Rathmann@tigertal.de>
2024-02-22 10:11:50 +0100

    obsolete code removed

--- examples/tabview/main.cpp
+++ examples/tabview/main.cpp
@@ -3,13 +3,8 @@
  *           SPDX-License-Identifier: BSD-3-Clause
  *****************************************************************************/
 
-#include "CustomSlider.h"
-#include "OtherSlider.h"
-
 #include <SkinnyShortcut.h>
 
-#include <QskAspect.h>
-#include <QskBoxBorderColors.h>
 #include <QskFocusIndicator.h>
 #include <QskLinearBox.h>
 #include <QskObjectCounter.h>
@@ -35,36 +30,6 @@ class Label : public QskTextLabel
     }
 };
 
-class SliderBox : public QskLinearBox
-{
-  public:
-    SliderBox( QQuickItem* parent = nullptr )
-        : QskLinearBox( Qt::Vertical, parent )
-    {
-        setMargins( 30 );
-        setSpacing( 50 );
-        setExtraSpacingAt( Qt::BottomEdge );
-
-        {
-            auto slider = new OtherSlider( this );
-
-            slider->setMinimum( 0 );
-            slider->setMaximum( 10 );
-            slider->setStepSize( 1 );
-        }
-
-        {
-            auto slider = new CustomSlider( this );
-
-            slider->setSnap( true );
-            slider->setMinimum( 0 );
-            slider->setMaximum( 2000 );
-            slider->setStepSize( 10 );
-            slider->setPageSize( 10 );
-        }
-    }
-};
-
 class TabView : public QskTabView
 {
   public:
@@ -78,16 +43,11 @@ class TabView : public QskTabView
                 const auto text = QStringLiteral( "Another Tab" );
                 addTab( text, new Label( text ) );
             }
-            else if ( i == 7 )
-            {
-                addTab( "Sliders", new SliderBox() );
-            }
             else
             {
                 const auto text = QString( "Tab %1" ).arg( i + 1 );
                 addTab( text, new Label( text ) );
             }
-
         }
 
         setTabEnabled( 2, false );
@@ -168,8 +128,6 @@ int main( int argc, char* argv[] )
     layoutBox->addItem( tabView );
 
     auto focusIndicator = new QskFocusIndicator();
-    focusIndicator->setObjectName( "FocusIndicator" );
-    focusIndicator->setBoxBorderColorsHint( QskFocusIndicator::Panel, Qt::red );
 
     QskWindow window;
     window.resize( 800, 600 );
